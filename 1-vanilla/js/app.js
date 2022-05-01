3d1bf7fcb19c059de3e62a9c15e45a669a08d808
moonji <43643902+moon729@users.noreply.github.com>
2022-05-01 11:28:05 +0900

    lecture view 요구사항
    lecture view 요구사항 파일 추가

--- 1-vanilla/js/app.js
+++ 1-vanilla/js/app.js
@@ -1,5 +1,6 @@
 import MainController from './Controllers/MainController.js'
 
-document.addEventListener('DOMContentLoaded', () => {
-    MainController.init()
+document.addEventListener('DOMContentLoaded', () => { 
+
+    MainController.init() // dom load 완료 시, maincontroller의 init() 실행 
 })
