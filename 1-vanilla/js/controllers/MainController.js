9c69b8efdf4b94d7250da68358e0f35a8c53e82e
moonji <43643902+moon729@users.noreply.github.com>
2022-05-01 18:05:57 +0900

    FormView2

--- 1-vanilla/js/controllers/MainController.js
+++ 1-vanilla/js/controllers/MainController.js
@@ -3,7 +3,12 @@ import FormView from '../views/FormView.js'
 const tag = '[MainController]'
 
 export default {
-  init() {
-    FormView.setup(document.querySelector('form'))
-  },
+    init() {
+        FormView.setup(document.querySelector('form'))
+            .on('@submit', e => this.onSubmit(e.detail.input))
+    },
+
+    onSubmit(input) {
+        console.log(tag, 'onSubmit()', input)
+    }
 }
--- 1-vanilla/js/views/FormView.js
+++ 1-vanilla/js/views/FormView.js
@@ -5,25 +5,29 @@ const tag = '[FormView]'
 const FormView = Object.create(View)
 
 FormView.setup = function (el) {
-  this.init(el)
-  this.inputEl = el.querySelector('[type=text]')
-  this.resetEl = el.querySelector('[type=reset')
-  this.showResetBtn(false)
-  this.bindEvents()
-  return this
+    this.init(el)
+    this.inputEl = el.querySelector('[type=text]')
+    this.resetEl = el.querySelector('[type=reset')
+    this.showResetBtn(false)
+    this.bindEvents()
+    return this
 }
 
 FormView.showResetBtn = function (show = true) {
-  this.resetEl.style.display = show ? 'block' : 'none'
+    this.resetEl.style.display = show ? 'block' : 'none'
 }
 
 FormView.bindEvents = function () {
-  this.on('submit', e => e.preventDefault())
-  this.inputEl.addEventListener('keyup', e => this.onKeyup(e))
+    this.on('submit', e => e.preventDefault())
+    this.inputEl.addEventListener('keyup', e => this.onKeyup(e))
 }
 
-FormView.onKeyup = function (e) {
-  this.showResetBtn(this.inputEl.value.length)
+FormView.onKeyup = function (e) { //키보드 동작에 대한 정의 
+    const enter = 13
+    this.showResetBtn(this.inputEl.value.length)
+    if(e.keyCode !== 13) return
+    //else.. keycode = 13이면 즉 enter 입력 시 input에 입력된 값 전달 
+    this.emit('@submit', {input: this.inputEl.value}) //key=input, value=inputEl.value 의 형태로 전달 
 }
 
 export default FormView
